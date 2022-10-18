117f698c06e3a22f025441c7c14656b05a4cc5aa
Rutuja Talekar <ritu1698talekar@gmail.com>
2022-10-17 23:40:18 -0400

    Membership test

--- bf.py
+++ bf.py
@@ -7,6 +7,7 @@ Created on Sun Oct  9 20:21:27 2022
 
 from cProfile import label
 import math
+from turtle import color
 import mmh3
 from bitarray import bitarray
 import sys
@@ -15,49 +16,51 @@ import matplotlib.pyplot as plt
 #from random import shuffle
 
 class BloomFilter(object):
-    def __init__(self, items_count, hash_count, bf_size):
-        self.items_count= items_count
+    def __init__(self, total_words, hash_count, bf_size):
+        self.total_words= total_words
         # number of hash functions to use
         self.total_hashs = hash_count
          # Size of bit array to use
-        self.size = bf_size
+        self.bf_size = bf_size
         # False possible probability in decimal
-        self.fp_prob = self.getFpProb()
+        self.fp_rate = self.getFpRate()
 		# Bit array of given size
-        self.bit_array = bitarray(self.size)
+        self.bit_array = bitarray(self.bf_size)
 		# initialize all bits as 0
         self.bit_array.setall(0)
         
     def reset(self):
         del self.bit_array[:]
-        print("Bit array has been reset", self.bit_array)
+        print("The bloom filter bit array has been reset", self.bit_array)
         return True
     
-    def add(self, item):
+    def insert(self, item):
         digests = []
         for i in range(self.total_hashs):
-			# i work as seed to mmh3.hash() function
-            digest = mmh3.hash(item, i) % self.size
+            digest = mmh3.hash(item, i) % self.bf_size  
             digests.append(digest)
-			# set the bit True in bit_array
             self.bit_array[digest] = True
     
-    def membershipCheck(self, item):
+    def testmembership(self, item):
         for i in range(self.total_hashs):
-            digest = mmh3.hash(item, i) % self.size
+            digest = mmh3.hash(item, i) % self.bf_size
             if self.bit_array[digest] == False:
                 return False
         return True
     
-    def getFpProb(self):
+    def getFpRate(self):
         #P(false positives ) = is given with a formula (1-e^(-kn/m))^k
-        kn = self.total_hashs * self.items_count
-        val = -kn/self.size
+        kn = self.total_hashs * self.total_words
+        val = -kn/self.bf_size
         P = (1-math.exp(val))**self.total_hashs
         return P
+    
+    
+def getOptimalHashFns(bf_size,total_words):
+        # Optimal k = (m/n) * lg(2)
+        return int((bf_size/total_words) * math.log(2))       
 
 def readDataSet():
-    #fin = open("wlist_match1.txt")
     with open("wlist_match12.txt") as file_in:
         lines = []
         for line in file_in:
@@ -74,79 +77,88 @@ def readCommands(filename):
     return bfSize,testData
     
 def plotGraph(num_of_words, dict):
-    
-    # plt.plot(num_of_words,fp_rate)
-    # plt.xlabel('Number of words')
-    # plt.ylabel('False positive rate')
-    
     fig,ax = plt.subplots()
     # for k,v in dict.items():
     #     ax.plot(num_of_words, v, label= k)
-    
+    plt.title("Bloom Filter Performance Analysis", weight="bold", color="blue")
     ax.plot(num_of_words, dict[2], color = 'green', label="H2")
     ax.plot(num_of_words, dict[4], color = 'red', label="H4")
     ax.plot(num_of_words, dict[8], color = 'blue', label="H8")
     ax.plot(num_of_words, dict[16], color = 'purple', label="H16")
-    
+    ax.set_xlabel("Number of words", weight = "bold")
+    ax.set_ylabel("False positive rate", weight = "bold")
+    plt.legend()
     plt.show()
     return True
 
+'''
+def plotGraphOptimalK(num_of_words, fp_rate):
+    plt.plot(num_of_words, fp_rate)
+    plt.title("Bloom Filter Performance Analysis", weight="bold", color="blue")
+    plt.xlabel("Number of words", weight = "bold")
+    plt.ylabel("False positive rate", weight = "bold")
+    plt.legend()
+    plt.show()
+    return True
+'''
 
 dataset = readDataSet()
 #m = bloom size, k = number of hash functions used, n = total number of words
 m,test_data = readCommands(sys.argv[1])
 n = [100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000, 25000]
 k = [2,4,8,16]
+#dict will store number of hash functions used : respective FP rate
 dict = {2:[],4:[], 8:[], 16:[]}
-#Storing values for graph
-fp_rates = []
 
-
-#1 k=2, m=16 n= 100,...........
-# for i in range(len(k)):
-#     for j in range(len(n)):
-#         bf = BloomFilter(n[j], k[i], m)
-#         fp_rates[j]= bf.getFpProb()
 
 for i in range(len(n)):
     for key,value in dict.items():
         bf= BloomFilter(n[i], key, m)
-        dict[key].append(bf.getFpProb())  
+        dict[key].append(bf.getFpRate())  
 
-print(dict)
-
-'''
-print("Total number of elements added in bloom filter:{}".format(n))
-print("Size of bit array:{}".format(bloomf.size))
-print("Number of hash functions:{}".format(bloomf.total_hashs))
-print("False positive Probability:{}".format(bloomf.fp_prob))
-#print("Test the following words:", test_data)
-#print("Bloom filter before adding words........")
-#print(bloomf.bit_array)
-'''
-
-'''
-#Using the best fp rate configurations for membership check
-bfTest = BloomFilter(n[-1], k[-1], m)
-for item in dataset:
-	bfTest.add(item)
-'''
-#Membership test
-
-# for item in test_data:
-#     if bfTest.membershipCheck(item):
-#         print("Item probably exists in bloom filter")
-#     else:
-#         print("Item definitely doesnt exists in bloom filter")
-
-
-
+#print(dict)
 #Plot graph
+print()
+print("The graph shows FP rates against the number of words for different count of hash functions for the bloom filter")
 plotGraph(n, dict)
+print()
+'''
+fp_rates = []
+#Plot optimal K graph
+for i in range(len(n)):
+    kOptimal = getOptimalHashFns(m,n[i])
+    bfOptimalK = BloomFilter(n[i],kOptimal, m)
+    fp_rates.append(bfOptimalK.getFpRate())
+plotGraphOptimalK(n, fp_rates)
+'''
 
 
-#Test rest function
-#print(bfTest.reset())
+
+
+
+
+#Using the best fp rate configurations for membership check
+bfTest = BloomFilter(n[-2], k[-2], m)      #Taking m = 10000 since it is giving balanced results
+for item in dataset:
+	bfTest.insert(item)
+
+print("Total number of words - ",bfTest.total_words)
+print("Total number of hash functions used -",bfTest.total_hashs)
+print("Size of bit array | bloom filter - ",bfTest.bf_size)
+print("Projected false positive rate - ",bfTest.getFpRate())
+print()
+
+#Membership test
+for item in test_data:
+    if bfTest.testmembership(item):
+        print("The following item could be a member - " + item)
+    else:
+        print("The following item is not a member - "+ item)
+
+#Reset function
+print()
+bfTest.reset()
+
 
 
 
